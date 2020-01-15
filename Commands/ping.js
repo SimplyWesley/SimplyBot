7e7d978a9a5222492b658e3a2f1d5fec9ade1e83
SimplyWesley <59846142+SimplyWesley@users.noreply.github.com>
2020-01-15 15:46:28 +0100

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,6 +1,7 @@
 module.exports.run = async(bot, message, args) => {
 
-    message.channel.send("Pong " + (message.createdTimestamp - Date.now()) + "ms"); 
+    // message.channel.send("Pong " + (message.createdTimestamp - Date.now()) + "ms"); 
+    message.channel.send("Pong " + (Date.now() - (message.createdTimestamp)) + "ms"); 
 
 }
 
